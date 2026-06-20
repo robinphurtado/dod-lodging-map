672877e1556f02544cd54cc4a7759aa35ba71cf0
Robin Hurtado <robin12898@hotmail.com>
2026-06-19 20:48:21 -0700

    decreased mobile search window a little more

--- src/components/SearchPanel/SearchPanel.jsx
+++ src/components/SearchPanel/SearchPanel.jsx
@@ -56,7 +56,7 @@ const PROPERTY_TYPES = [
         width: { xs: "auto", sm: 390 },
         maxWidth: "calc(100vw - 32px)",
 
-        maxHeight: { xs: "50vh", md: "none" },
+        maxHeight: { xs: "40vh", md: "none" },
         overflowY: { xs: "auto", md: "visible" },
 
         borderRadius: { xs: 1, md: 1 },
